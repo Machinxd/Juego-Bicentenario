b833ca435c956a6a44bb453f9bfeff8a4ef15293
Machinxd <159960018+Machinxd@users.noreply.github.com>
2025-07-11 19:01:17 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
 function ir(pagina) {
-  const sonido = document.getElementById("click-sound");
-  sonido.currentTime = 0;
-  sonido.play();
-  setTimeout(() => {
-    window.location.href = pagina;
-  }, 300); // para que suene antes de ir
+  window.location.href = pagina;
 }
